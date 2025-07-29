70ccc5aa378681e8a03be7053aa0d7fe4bd8fbca
shivakumar2006 <official.shivakumar06@gmail.com>
2025-07-29 19:06:31 +0530

    commit

--- src/pages/content.jsx
+++ src/pages/content.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import image from "../assets/gym.jpg";
 import { CgGym } from "react-icons/cg";
+import personal from "../assets/personal.jpg";
+import personal2 from "../assets/personal2.jpg";
+import weight from "../assets/weight.jpg";
+import cardio from "../assets/cardio.jpg";
 
 const content = () => {
   return (
@@ -65,23 +69,62 @@ const content = () => {
         </div>
 
         <div className='w-screen h-full flex flex-row justify-center items-center'>
-            <div className='w-200 h-full border border-white flex flex-col justify-center items-center'>
+            <div className='w-200 h-full flex flex-col justify-center items-center'>
                 <div className='w-full h-90 flex flex-col justify-center items-center'>
-                    <p className='text-[130px] mr-50 font-extrabold text-white'>﹥<span className='text-[#C7FF39]'>﹥</span></p>
-                    <p className='text-4xl font-bold text-white'>PERSONAL <span className='text-[#C7FF39]'>TRAINING</span></p>
+                    <p className='text-[130px] mr-35 font-extrabold text-white'>﹥<span className='text-[#C7FF39]'>﹥</span></p>
+                    <p className='text-4xl ml-10 font-bold text-white'>PERSONAL <span className='text-[#C7FF39]'>TRAINING</span></p>
                 </div>
                 <div className='w-full h-100 flex justify-center items-center'>
-                    <div className='w-80 h-70 rounded-2xl bg-black/20 flex flex-col justify-center items-center'>
+                    <div className='w-80 h-70 rounded-2xl bg-black/20 flex flex-col justify-center items-center gap-10'>
                         <p className='text-white'>Experience a full personal training <br /> sessions, free of charge</p>
-                        <button>
-                            
+                        <button className='w-40 mr-23 h-12 text-lg font-medium text-black bg-[#C7FF39] rounded-xl cursor-pointer'>
+                            See Price
                         </button>
                     </div>
                 </div>
             </div>
-            <div className='w-full h-full border border-pink-500 flex flex-col justify-center items-center'>
-
+            <div className='w-full h-full flex flex-row justify-center items-center'>
+                <div className='w-1/2 h-full ml-10'>
+                    <img 
+                        src={personal}
+                        alt='personal'
+                        className='w-80 h-120 rounded-2xl transition-transform duration-300 shadow-2xl hover:scale-103'
+                    />
+                </div>
+                <div className='w-1/2 h-full flex flex-col'>
+                    <img 
+                        src={personal2}
+                        className='w-90 h-60 mb-5 rounded-2xl transition-transform duration-300 shadow-2xl hover:scale-103'
+                    />
+                    <p className='text-white font-light'>
+                        Whether you looking for build muscle, lose fat, or <br /> improve mobility, every session is customized to fit your <br /> needs and fitnes level
+                    </p>
+                    <div className='w-30 h-30 rounded-2xl bg-[#C7FF39] mt-5 flex flex-col justify-center items-center gap-2'>
+                        <p className='font-extrabold text-4xl'>10+</p>
+                        <p className='font-extralight'>persona <br /> Trainers</p>
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
+
+        {/* programs */}
+    <div className='w-auto min-h-screen bg-[#1D1F20] flex flex-col'>
+        <div className='w-full h-auto mt-20 flex flex-row justify-around items-center gap-50'>
+            <p className='text-4xl text-white font-bold'>PROGRAMS THAT <span className='text-[#C7FF39]'>FIT YOU</span></p>
+            <p className='text-[12px] font-extralight text-white'>Whether you are just starting or levelling up , our coaching is build around <br /> your needs, not a one size-fits-all plan.</p>
+        </div>
+        <div className='w-full mt-10 flex flex-row justify-around items-center'>
+            <img 
+                src={weight}
+                alt="lifting"
+                className='w-160 h-80 rounded-2xl object-cover transition-transform duration-300 hover:scale-103'
+            />
+            <img 
+                src={cardio}
+                alt="cardio"
+                className='w-110 h-80 rounded-2xl object-cover transition-transform duration-300 hover:scale-103'
+            />
         </div>
     </div>
     </>
